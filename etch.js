af9ec00fe3149b49706899df3a5af7a333e24d9c
Berriano <beresford2102@yahoo.com.au>
2021-04-27 13:49:16 +1000

    Add

--- etch.js
+++ etch.js
@@ -1,41 +1,78 @@
 //select #grid-container
 
+
+
 const container = document.querySelector('#grid-container');
-
-
-
 
 
 //function to create multiple divs
 
 function boxes (){
 
-
 //loop for multiple boxes
 
+let x=0;
 
     for(x=0;x<256;x++){
 
 
-// create new div - box 1
+// create new div-box
 
-const div1 = document.createElement('div');
+
+const div1 = document.createElement('button');
 div1.classList.add('box');
 
-// add content to box
 
-div1.textContent = "box";
+//add box to the html
 
-//add box 1 to the html
 
 container.appendChild(div1);
-    }
+
+
+//change box color
 
 
 
+const divCol = document.querySelectorAll('button');
+
+divCol.forEach((button) => {
+
+    button.addEventListener('mouseover', function (colChange)  {colChange.target.style.background = 'blue';}
+    
+    );
+ })
+
+// change color back
 
 
+    //divCol.forEach((button) => {
 
+        //  button.addEventListener("mouseout", function (changeBack)  {changeBack.target.style.background = "white";}
+    
+   /// );
+ //})
+
+
+  }
 
 }
+    
+    
+    
 boxes();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
