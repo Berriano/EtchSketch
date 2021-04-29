34f208d6f6e4a7ede83f1737b2752d4b9c55743e
Berriano <beresford2102@yahoo.com.au>
2021-04-29 11:52:58 +1000

    Add

--- etch.js
+++ etch.js
@@ -1,80 +1,97 @@
-//select #grid-container
 
+/// grid size prompt box function ///
+
+
+let newgrid = gridSize();
+
+ function gridSize(){
+
+    let wGrid=0;
+    let hGrid=0;
+
+    wGrid = prompt("How many boxes wide?");
+    hGrid = prompt("how many boxes high?");
+
+    return wGrid * hGrid; 
+}
+
+
+                        /// select #grid-container ///
 
 
 const container = document.querySelector('#grid-container');
 
 
-//function to create multiple divs
+
+                            //////boxes function///////
+
 
 function boxes (){
 
-//loop for multiple boxes
+
+                                    /// loop  ///
 
 let x=0;
 
-    for(x=0;x<256;x++){
+    for(x=0;x<newgrid;x++){
 
 
-// create new div-box
+                            /// create new div-box ///
+
 
 
 const div1 = document.createElement('div');
 div1.classList.add('box');
-
-
-//add box to the html
-
-
 container.appendChild(div1);
 
 
+                    ///change box color (included in loop) ///
 
-// change box color
+
 
 const allGrid = document.querySelectorAll("div.box");
 
 allGrid.forEach((div) => {
 
-    div.addEventListener('mouseover', function (colChange)  {colChange.target.style.background = 'blue';}
-    
-    );
+    div.addEventListener('mouseover', function (colChange){
+        colChange.target.style.background = 'blue';
+    }
+   );
  })
 
-
 }
+                /////////////end of loop/////////////
 
 
-//clear button
+
+ 
+
+
+
+                        /// clear button ///
 
 const clearBtn = document.querySelector("#clear-btn");
 const clearGrid = document.querySelectorAll(".box");
  
-
-
 clearGrid.forEach((div) => {
 clearBtn.addEventListener('click',function(){div.style.background = 'white';})
+})
 
-
-    })
-
+clearBtn.addEventListener('click', gridSize)
 }
-  
 
+
+
+        ///////////////end of boxes function/////////////////
+
+ 
+//run boxes function and check gridsizre value
 boxes();
 
+console.log(gridSize());
 
 
-
-
-//function for promt box
-
-function pBox(){
-    let userGrid = prompt("How many per side for new grid?");
-    }
-
-
-
+                   /////////////// end ///////////////
 
 
 
