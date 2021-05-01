8c7b12c2e3980682709bb63ab55568397cb8c4f6
Berriano <beresford2102@yahoo.com.au>
2021-05-01 15:48:15 +1000

    grid size correct

--- etch.js
+++ etch.js
@@ -1,5 +1,7 @@
-
-                        //////create grid container////////
+                     
+                       
+                       
+                       //////create grid container////////
 
 const container = document.querySelector('.container'); 
 const gridCont = document.createElement('div');
@@ -20,25 +22,39 @@ function boxes (){
                                 /// grid size prompt box ///
 
 
-let  wGrid = prompt("How many boxes wide?");
-let  hGrid = prompt("how many boxes high?");
-let gridSize = wGrid * hGrid;
-                                   
+let  wGrid = prompt("How many boxes squared?");
+let gridSize = wGrid * wGrid;
+
+
+
+                   /////////////size grid to boxes amount/////////////////
+
+
+
+
+  let gridCss = document.getElementById('grid-container');
+  gridCss.style.gridTemplateColumns = `repeat(${wGrid}, 1fr)`;
+  gridCss.style.gridTemplateRows = `repeat(${wGrid}, 1fr)`;
+
+
+ 
+
                                ///////// loop  ////////////
 
-
-
-    for(let x=0;x<gridSize;x++){
+    
+     for(let x=0;x<gridSize;x++){
 
 
                   ////////// to create new div-box class 'box'/////////
 
 
 
-const gridContainer = document.querySelector('#grid-container');
+const gridContainer = document.getElementById('grid-container');
 const div1 = document.createElement('div');
 div1.classList.add('box');
 gridContainer.appendChild(div1);
+
+                
 
        
                        ////////// ///change box color/////////////
@@ -53,7 +69,8 @@ allGrid.forEach((div) => {div.addEventListener('mouseover', function (colChange)
 
                           /////////////end of loop/////////////
 
-
+                        
+                            
 console.log(gridSize);
 
 }
@@ -74,8 +91,7 @@ console.log(gridSize);
        console.log(element);
      }
 })            
-
-                      //////////////// end of clear /////////////////    
+  
 
 
                     /////////////////restart by click/////////////////
@@ -83,6 +99,20 @@ console.log(gridSize);
 
 const restart = document.getElementById('clear-btn');
 restart.addEventListener('click', boxes);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
                //////////////////////////// end ////////////////////////////////
