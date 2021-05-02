0d7a942cb94cd54ecf9d4f18a51cff9b5da35d89
Berriano <beresford2102@yahoo.com.au>
2021-05-02 12:17:34 +1000

    Final js

--- etch.js
+++ etch.js
@@ -4,6 +4,7 @@
 
 const container = document.querySelector('.container'); 
 const gridCont = document.createElement('div');
+
 gridCont.classList.add('grid-container');
 gridCont.setAttribute('id', 'grid-container')
 gridCont.removeAttribute('class','grid-container')
@@ -14,12 +15,12 @@ container.appendChild(gridCont);
 
 boxes();
 
-                                //////boxes function///////
+                        ////////////// boxes function //////////////
 
 
 function boxes (){
 
-                                /// grid size prompt box ///
+                       //////////// grid size prompt box ////////////////
 
 
 let  wGrid = prompt("How many boxes squared?");
@@ -27,16 +28,17 @@ let gridSize = wGrid * wGrid;
 
 
 
-                   /////////////size grid to boxes amount/////////////////
+                   ///////////// size grid to boxes amount /////////////////
 
 
   let gridCss = document.getElementById('grid-container');
+
       gridCss.style.gridTemplateColumns = `repeat(${wGrid}, 1fr)`;
       gridCss.style.gridTemplateRows = `repeat(${wGrid}, 1fr)`;
 
 
 
-                              ///////// loop  ////////////
+                      ///////////// loop grid creation //////////////
 
     
        for(let x=0;x<gridSize;x++){
@@ -48,19 +50,22 @@ let gridSize = wGrid * wGrid;
 
 const gridContainer = document.getElementById('grid-container');
 const div1 = document.createElement('div');
+
 div1.classList.add('box');
 gridContainer.appendChild(div1);
 
                 
        
-                       ////////// ///change box color/////////////
+                       ///////////// change box color /////////////
 
 
 const allGrid = document.querySelectorAll(".box");
+
 allGrid.forEach((div) => {div.addEventListener('mouseover', function (colChange){
                                               colChange.target.style.background = 'blue';});
   })
-}
+
+
 
                           /////////////end of loop//////////////
 
@@ -68,9 +73,9 @@ allGrid.forEach((div) => {div.addEventListener('mouseover', function (colChange)
 
                       ///////////////end of boxes function//////////////
 
-
+}
             
-              //////////////////////clear button function/////////////////////
+              //////////////////////// clear button ////////////////////////
 
 
     const clBtn = document.getElementById('clear-btn');
@@ -83,11 +88,11 @@ allGrid.forEach((div) => {div.addEventListener('mouseover', function (colChange)
 })            
   
 
-
                     /////////////////restart by click/////////////////
 
 
 const restart = document.getElementById('clear-btn');
+
 restart.addEventListener('click', boxes);
 
 
@@ -96,7 +101,7 @@ restart.addEventListener('click', boxes);
 
 
 
-
+              
 
 
 
