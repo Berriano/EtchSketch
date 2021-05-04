c63a24a8fd4b741198be693ca16684a2b147e7e1
Berriano <beresford2102@yahoo.com.au>
2021-05-04 15:13:11 +1000

    Slider

--- black-etch.js
+++ black-etch.js
@@ -11,21 +11,58 @@ gridCont.removeAttribute('class','grid-container')
 container.appendChild(gridCont);
 
 
+/////////////////////////////////// SLIDER ///////////////////////////////////////
 
 
 
-                        ////////////// boxes function //////////////
+
+let slideValue = document.getElementById('slide-bar');
+let slideText = document.getElementById('slide-text');
+
+slideText.innerHTML = slideValue.value;
+
+slideValue.oninput = function (){
+
+    slideText.innerHTML = slideValue.value;
+
+}
+
+
+
+let defaultValue = slideValue.defaultValue;
+let currentValue = slideValue.value;
+
+wGrid = 10;
+
+
+console.log("default" + defaultValue);
+console.log("current" + currentValue);
+
+
+
+
+////////////////////////////////// SLIDER END ////////////////////////////////////  
+
+
+
+
+
+                       ////////////// boxes function //////////////
 
 
 function boxes (){
 
+
+    
+
+
                        //////////// grid size prompt box ////////////////
 
 
-let  wGrid = prompt("How many boxes squared?");
-let gridSize = wGrid * wGrid;
+//let  wGrid = prompt("How many boxes squared?");
+//let gridSize = wGrid * wGrid;
 
-                 ///////////// size grid from prompt /////////////////
+                //////////////// size grid from prompt ///////////////////
 
 
   let gridCss = document.getElementById('grid-container');
@@ -34,11 +71,13 @@ let gridSize = wGrid * wGrid;
       gridCss.style.gridTemplateRows = `repeat(${wGrid}, 1fr)`;
 
 
+
+
  const gridContainer = document.getElementById('grid-container');
 
                        ///////////// loop grid creation //////////////
 
-     for(let x=0;x<gridSize;x++){
+     for(let x=0;x<currentValue;x++){
 
 
                   ////////// create new div-box class 'box'/////////
@@ -47,33 +86,28 @@ const div1 = document.createElement('div');
 
 div1.classList.add('box');
 gridContainer.appendChild(div1);
-
 }           
                                 /////////////end of loop//////////////
-
 
                              ///////////// change box color /////////////
 
 const allGrid = document.querySelectorAll(".box");
 
   allGrid.forEach((div) => {div.addEventListener('mouseover', function (colChange){
-                                        colChange.target.style.background = genRandCol();});
+                                        colChange.target.style.background = "black";});
                                                                
   })
  }
-                       ///////////////end of boxes function//////////////   
 
+                       ///////////////end of boxes function//////////////   
 
      /////////////////////////////// generate random color /////////////////////////
 
        function genRandCol(){
          let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);    
              return randomColor;
-                               
-   }     
-       /////////////////////////////// end of random color ////////////////////////////
-
-     
+ }     
+         
               //////////////////////// clear button ////////////////////////
 
 
@@ -95,7 +129,6 @@ const restart = document.getElementById('clear-btn');
 restart.addEventListener('click', boxes);
 
 
-          ////////////////////call boxes function to start////////////////////
     
 boxes();
 
